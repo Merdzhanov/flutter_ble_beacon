e78524d8b2659d1da2d3aa1806eb9d6087885d1f
Konstantin Merdzhanov <kmerdjanov@gmail.com>
2020-04-16 15:34:00 +0300

    Draft version rebuilded

---
+++ android/app/src/main/kotlin/com/konstantin/merdzhanov/ble_beacon_broadcast_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.konstantin.merdzhanov.ble_beacon_broadcast_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
